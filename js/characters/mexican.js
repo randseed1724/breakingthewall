faec9e01072b79601a55c48051c71e14f265c8da
Alejandro <ajimenez1724@gmail.com>
2017-03-18 16:43:34 -0400

    commit 5

--- js/characters/mexican.js
+++ js/characters/mexican.js
@@ -4,12 +4,27 @@ var totalSky;
 
 Column.prototype.handleClick = function(event) {
   var target = $(event.target);
+  var blockReplace =  $(event.target).parent() ;
+
+  console.log(blockReplace);
+
+  var sky = "<div class='sky' id='sky_' >";
 
    skySpace =  target.parent();
    totalSky =  skySpace.find(".sky").length;
 
    console.log(totalSky);
 
-   target.removeClass('block');
-   target.addClass('sky');
+
+  //  target.addClass('sky');
+  //  target.toggle();
+
+  //  target.detach();
+  blockReplace.prepend(sky);
+
+  target.hide();
+  // target.addClass('sky');
+
 };
+
+//
